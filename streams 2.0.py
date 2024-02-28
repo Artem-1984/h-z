753484fa2b6d50f4e453d3b8274ada666dffe038
Artem-1984 <118545099+Artem-1984@users.noreply.github.com>
2024-02-28 10:11:40 +0300

    Add files via upload

---
+++ streams 2.0.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import threading
+
+
+# def preservation_directories(old_path, new_path):
+#     print('Начало потока')
+#     shutil.copytree(old_path, new_path)
+#
+#     print('Конец потока')
+#
+#
+# flag = True
+# while flag:
+#     path = input("Введите директорию: ")
+#     if os.path.exists(path):
+#         old_path = path
+#         flag = False
+#         print(f"Перевод в директорию: {path}")
+#     else:
+#         print("Директория не существует")
+#         continue
+#     new_path = input("Введите новую директорию: ")
+#
+# th1 = threading.Thread(target=preservation_directories, args=(old_path, new_path))
+# th1.start()
+
+
+
+
+
+
+
+
+
+
